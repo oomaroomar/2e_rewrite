78ffaa78b7a44ae7e0d100985dc2900eddadd958
oomaroomar <omar.mayani@gmail.com>
2024-12-09 17:00:52 +0200

    added some shadCN stuff: including shadCN multiselector which is an addon. Had to make some manual changes because the component was borked but looks good now.

--- src/app/homebrew/page.tsx
+++ src/app/homebrew/page.tsx
@@ -1,15 +1,34 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Input } from "~/components/ui/input";
+import MultiSelector, { type Option } from "~/components/ui/multipeSelector";
 
 export default function App() {
+  const [value, setValue] = useState<Option[]>([]);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
+    control,
   } = useForm();
   const onSubmit = (data: unknown) => console.log(data);
+
+  const OPTIONS: Option[] = [
+    { label: "nextjs", value: "Nextjs" },
+    { label: "React", value: "react" },
+    { label: "Remix", value: "remix" },
+    { label: "Vite", value: "vite" },
+    { label: "Nuxt", value: "nuxt" },
+    { label: "Vue", value: "vue" },
+    { label: "Svelte", value: "svelte" },
+    { label: "Angular", value: "angular" },
+    { label: "Ember", value: "ember" },
+    { label: "Gatsby", value: "gatsby" },
+    { label: "Astro", value: "astro" },
+  ];
 
   console.log(watch("example")); // watch input value by passing the name of it
   console.log("rerender");
@@ -18,7 +37,32 @@ export default function App() {
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <div className="flex w-full flex-col gap-5 px-10">
+        <p className="text-primary">
+          Your selection: {value.map((val) => val.label).join(", ")}
+        </p>
+        <Controller
+          control={control}
+          name="test"
+          render={({ field }) => (
+            <MultiSelector
+              {...field}
+              defaultOptions={OPTIONS}
+              placeholder="Select frameowkrs you like..."
+              hidePlaceholderWhenSelected={true}
+              emptyIndicator={
+                <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                  no results found.
+                </p>
+              }
+            />
+          )}
+        />
+      </div>
+      <div className="p-4">
+        <Input defaultValue="test" {...register("example")} />
+      </div>
+      {/* <input defaultValue="test" {...register("example")} /> */}
 
       {/* include validation with required or other standard HTML validation rules */}
       <input {...register("exampleRequired", { required: true })} />
