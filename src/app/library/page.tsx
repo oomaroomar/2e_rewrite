2abef06e8c12d32b79ff856c8758daa5181c30da
oomaroomar <omar.mayani@gmail.com>
2025-01-11 20:51:32 +0200

    fixed buil error

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -9,51 +9,54 @@ import CreateCharacterForm from "./_components/CreateCharacterForm";
 import CharacterList from "./_components/CharacterList";
 import SpellPageWrapper from "./_components/SpellPageWrapper";
 import PreparedLevelSpellTable from "./_components/PreparedLevelSpellTable";
+import { Suspense } from "react";
 
 export default async function LibraryPage() {
   void api.character.getMyCharacters.prefetch();
 
   return (
-    <div className="h-full w-full">
-      <ResizablePanelGroup className="w-full" direction="horizontal">
-        <ResizablePanel
-          style={{ overflow: "auto" }}
-          className="hidden overflow-auto pb-8 md:flex"
-          defaultSize={20}
-        >
-          <ResizablePanelGroup className="w-full" direction="vertical">
-            <ResizablePanel defaultSize={30}>
-              <Tabs
-                defaultValue="characters"
-                className="grid w-full grid-rows-[auto,1fr]"
-              >
-                <TabsList className="rounded-none">
-                  <TabsTrigger value="characters">Characters</TabsTrigger>
-                  <TabsTrigger value="books">Books</TabsTrigger>
-                </TabsList>
-                <TabsContent value="characters">
-                  <CreateCharacterForm />
-                  <CharacterList />
-                </TabsContent>
-                <TabsContent value="books">under construction</TabsContent>
-              </Tabs>
-            </ResizablePanel>
-            <ResizableHandle className="" />
-            <ResizablePanel className="p-4" defaultSize={70}>
-              <h1 className="text-lg font-bold">Prepared Spells</h1>
-              <PreparedLevelSpellTable />
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </ResizablePanel>
-        <ResizableHandle className="" />
-        <ResizablePanel
-          style={{ overflow: "auto" }}
-          className="overflow-auto pb-8"
-          defaultSize={80}
-        >
-          <SpellPageWrapper />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-full w-full">
+        <ResizablePanelGroup className="w-full" direction="horizontal">
+          <ResizablePanel
+            style={{ overflow: "auto" }}
+            className="hidden overflow-auto pb-8 md:flex"
+            defaultSize={20}
+          >
+            <ResizablePanelGroup className="w-full" direction="vertical">
+              <ResizablePanel defaultSize={30}>
+                <Tabs
+                  defaultValue="characters"
+                  className="grid w-full grid-rows-[auto,1fr]"
+                >
+                  <TabsList className="rounded-none">
+                    <TabsTrigger value="characters">Characters</TabsTrigger>
+                    <TabsTrigger value="books">Books</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="characters">
+                    <CreateCharacterForm />
+                    <CharacterList />
+                  </TabsContent>
+                  <TabsContent value="books">under construction</TabsContent>
+                </Tabs>
+              </ResizablePanel>
+              <ResizableHandle className="" />
+              <ResizablePanel className="p-4" defaultSize={70}>
+                <h1 className="text-lg font-bold">Prepared Spells</h1>
+                <PreparedLevelSpellTable />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+          <ResizableHandle className="" />
+          <ResizablePanel
+            style={{ overflow: "auto" }}
+            className="overflow-auto pb-8"
+            defaultSize={80}
+          >
+            <SpellPageWrapper />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </Suspense>
   );
 }
